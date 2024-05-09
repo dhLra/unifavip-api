2b035f2bfc9004d85ff5e1b1e810d1922abf7bb4
Dhiego <dhiegolira11@gmail.com>
2024-05-08 22:46:22 -0300

    fixing routes

--- src/routes/auth/auth.routes.ts
+++ src/routes/auth/auth.routes.ts
@@ -1,1 +1,12 @@
-///ARQUIVO DE ROTAS PARA AUTENTICAÇÃO
+import express from "express";
+import AuthController from "../../controllers/auth/auth.controller";
+
+const router = express.Router();
+
+const authControllerInstace = new AuthController;
+
+router
+    .post("/login", authControllerInstace.authenticator)
+
+
+export default router;
--- src/routes/routes.index.ts
+++ src/routes/routes.index.ts
@@ -1,12 +1,7 @@
-import express from "express";
-import AuthController from "../controllers/auth/auth.controller";
-
-const router = express.Router();
-
-const authControllerInstace = new AuthController;
-
-router
-    .post("/login", authControllerInstace.authenticator)
+import authRoutes from "./auth/auth.routes"
 
 
-export default router;
+const routes = require('express').Router();
+const prefix = '/api/v1'
+
+routes.use(`${prefix}/auth`, authRoutes);
